475f051682f67b2b4afdb7e3ff7b52d6a9ff2bb1
matteo_miggiano <mmi@soreco.ch>
2022-01-20 09:00:14 +0100

    erst eingaben

---
+++ main.java
@@ -0,0 +1,15 @@
+// 1.Anmeldung und Verifizierung
+// 2.Interface Bildschirm
+// 3. Turniere generieren
+//    3.1 Verschiedene Sportarten
+// 4. turnier übersicht
+//    4.1 Turnier Ansicht 
+// 5. Admin Klasse & User Klasse
+// 6. Turnier PDF drucken
+
+
+public class main {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
